075450c43749be9678ac7693cc7a057c8faa8c1b
Steven Mycynek <steven.mycynek@autodesk.com>
2017-03-23 09:01:49 -0400

    fix bounds inconsistency, add tests

--- AreaUnderCurve.Tests/AreaTests.cs
+++ AreaUnderCurve.Tests/AreaTests.cs
@@ -37,5 +37,28 @@ namespace AreaUnderCurve.Tests
             var area = AreaUnderCurve.Core.AreaUnderCurve.Calculate(polynomial, bounds, algorithm);
             Assert.Equal(area, 0, 2);
         }
+
+        [Fact]
+        public void Test_Simple_Area_4()
+        {
+
+            var bounds = new Bounds(-5, 5, 1);
+            var polynomial = new Polynomial(new SortedDictionary<double, double> { [3] = 1 }); //# f(x) = x^3
+            var algorithm = Algorithms.GetAlgorithm("Midpoint");
+            var area = AreaUnderCurve.Core.AreaUnderCurve.Calculate(polynomial, bounds, algorithm);
+            Assert.Equal(area, 0, 2);
+        }
+
+        [Fact]
+        public void Test_Simple_Area_5()
+        {
+
+            var bounds = new Bounds(-5, 5, .01);
+            var polynomial = new Polynomial(new SortedDictionary<double, double> { [3] = 1 }); //# f(x) = x^3
+            var algorithm = Algorithms.GetAlgorithm("Midpoint");
+            var area = AreaUnderCurve.Core.AreaUnderCurve.Calculate(polynomial, bounds, algorithm);
+            Assert.Equal(area, 0, 2);
+        }
+
     }
 }
